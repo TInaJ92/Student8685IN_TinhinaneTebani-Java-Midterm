959cc4515d2ffc72414fa4853434fb9db58c1dc9
TInaJ92 <tinatebany@gmail.com>
2023-06-01 22:51:24 -0400

    DuplicateWord

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -1,5 +1,8 @@
 package string_problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateWord {
 
     /** INSTRUCTIONS
@@ -8,9 +11,32 @@ public class DuplicateWord {
      */
 
     public static void main(String[] args) {
-        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        String inputString = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
         // Implement here
+        String[] words = inputString.toLowerCase().split(" ");
+
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        for (String word :words) {
+            if (wordCountMap.containsKey(word)){
+                wordCountMap.put(word, wordCountMap.get(word) + 1);
+            } else {
+
+                wordCountMap.put(word, 1);
+            }
+        }
+
+            System.out.println("Duplicate words and occurrences:");
+        for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+        if (entry.getValue() > 1) {
+            System.out.println(entry.getKey() + ": " + entry.getValue() + " occurrences");
+        }
+    }
+    int totalLength = 0;
+        for (String word : words) {
+        totalLength += word.length();
+    }
+    double averageLength = (double) totalLength / words.length;
 
     }
 
