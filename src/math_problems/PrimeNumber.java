ba4fe85154dffdea3bdc3e09613d4e61a7574bdb
TInaJ92 <tinatebany@gmail.com>
2023-06-02 17:17:45 -0400

    PrimeNumber

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -1,5 +1,7 @@
 package math_problems;
 
+import java.util.Arrays;
+
 public class PrimeNumber {
 
     /** INSTRUCTIONS
@@ -8,10 +10,31 @@ public class PrimeNumber {
      *
      * BONUS: Figure out how to improve algorithmic efficiency
      */
+    public static void printPrimeNumbers(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
 
 
+        // Mark 0 and 1 as non-prime
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for (int i = 2; i * i <= n; i++) {
+
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
     public static void main(String[] args) {
-
+        int n = 1000000;
+        printPrimeNumbers(n);
     }
 
 }
