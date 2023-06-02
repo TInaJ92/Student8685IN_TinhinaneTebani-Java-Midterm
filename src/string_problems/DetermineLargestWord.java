251a7f1b2d218a421c92c0324e3b32fc0aa5cbe4
TInaJ92 <tinatebany@gmail.com>
2023-06-01 22:43:10 -0400

    DetermineLargestWord

--- src/string_problems/DetermineLargestWord.java
+++ src/string_problems/DetermineLargestWord.java
@@ -15,15 +15,27 @@ public class DetermineLargestWord {
         String s = "Human brain is a biological learning machine";
         Map<Integer, String> wordNLength = findTheLargestWord(s);
 
-
+        System.out.println(wordNLength + ";");
     }
 
     public static Map<Integer, String> findTheLargestWord(String wordGiven) {
         Map<Integer, String> map = new HashMap<Integer, String>();
-        String st = "";
-
+        String st = wordGiven;
         // Implement here
-
+        String[] words = st.split(" ");
+        int maxLength = 0;
+        for (String word : words) {
+            int length = word.length();
+            if (length > maxLength) {
+                maxLength = length;
+            }
+        }
+        for (String word : words) {
+            int length = word.length();
+            if (length == maxLength) {
+                map.put(length, word);
+            }
+        }
         return map;
-    }
+}
 }
